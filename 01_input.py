3807ba8e5109bac064fef1f2296008a409a09234
Alfat393CSE <alfattasnimhasan@gmail.com>
2025-09-27 22:42:49 +0600

    Practice python

---
+++ 01_input.py
@@ -0,0 +1,7 @@
+a = input ("Enter your value: ") # input function always takes string value
+b = input ("Enter your value: ")
+
+print ("Number a is :", a)
+print ("Number b is :", b)
+
+print ("Sum of a and b is :", int(a) + int(b)) # converting string to integer using int() function
